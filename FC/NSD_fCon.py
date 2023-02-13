6fa2933a3c482b92f9fb7a11206eec6e2daf30cf
Maya Yablonski <mayayab@gmail.com>
2023-02-13 14:30:37 -0800

    replaced zeros with NaNs in correlation maps

--- FC/NSD_fCon.py
+++ FC/NSD_fCon.py
@@ -33,7 +33,7 @@ from nsdcode.nsd_output import nsd_write_fs
 from nsdcode.utils import makeimagestack
 
 
-# In[3]:
+# In[14]:
 
 
 # List all paths
@@ -42,7 +42,8 @@ if platform.system() == "Darwin":
     basepath = '/Users/mayayablonski/Sherlock/groupScratch/'
 elif platform.system() == "Linux":
     basepath = '/scratch/groups/jyeatman/'
-
+    basepath = '/oak/stanford/groups/jyeatman/'
+    
 basedir = basepath + 'NSD/ppdata/'
 fsdir = basepath + 'NSD/nsddata/freesurfer/'
 analysisdir = os.path.dirname(os.path.dirname((basedir))) + '/analysis/'
@@ -53,13 +54,13 @@ nsd = NSDmapdata(nsd_path)
 print(basedir)
 
 
-# In[4]:
+# In[15]:
 
 
 # Set parameters
 clobber = False
 
-hemi='rh'
+hemi='lh'
 seed_hemi = 'lh'
 
 droptp = [0,1,2,3,4,5]
@@ -73,10 +74,11 @@ if new_labels:
     roi_names = ['vwfa1','vwfa2','IFSwords','IPSwords']
 
 roi_names = ['VWFA1','VWFA2','IFSwords','IPSwords']
-faces = False
+
+faces = True
 if faces:
     roi_names = ['FFA1_lh','FFA2_lh']
-
+    
 # Denoising
 confound_list = ["WMe4","CSF","GSR",'trans_y','trans_x','trans_z', 'rot_x','rot_y','rot_z']
 use_conf_diff = True
@@ -100,7 +102,7 @@ seed_targetspace = seed_hemi+'.layerB2'
 interpmethod = 'cubic'
 
 
-# In[5]:
+# In[16]:
 
 
 # Syntax to run on subset of data for debugging
@@ -109,40 +111,42 @@ interpmethod = 'cubic'
 # runs = glob.glob(basedir + subs[0] + '/func1pt8mm/timeseries/timeseries*nii.gz')
 # runs.sort()
 # print('Found ' + str(len(runs)) + ' runs for sub ' + subs[0])
-subs=subs[7:8]
-print( subs)
+# subs=subs[0:1]
+subs
 
 
-# In[6]:
+# In[28]:
 
 
 for s,sub in enumerate(subs):
     fssub = fsdir + sub
     bolddir = basedir + sub + '/func1pt8mm/timeseries/'
     motiondir = basedir + sub + '/func1pt8mm/motion/'
-
+    
     surf = surface.load_surf_mesh(fssub + '/surf/'+hemi+'.white')
     labels = surface.load_surf_data(fssub + '/label/'+hemi+'.floc-words.mgz')
     curv = surface.load_surf_data(fssub + '/surf/'+hemi+'.curv')
-
+    
     runs = glob.glob(bolddir + '/timeseries*nii.gz')
     runs.sort()
     print('Found ' + str(len(runs)) + ' runs for sub ' + sub)
-    roi_zmap = np.zeros([surf.coordinates.shape[0], len(runs),len(roi_names)])
-
-    for r, run in enumerate(runs):
-
+    # Changed zeros to NaNs for more accurate averaging
+    roi_zmap = np.empty([surf.coordinates.shape[0], len(runs),len(roi_names)])
+    roi_zmap[:] = np.NaN
+    
+    for r, run in enumerate(runs[0:2]):
+        
         # load presaved confounds - WM and CSF regressors
         confound_file = str.replace(run,'timeseries','conf')
         confound_file = str.replace(confound_file,'.nii.gz','.tsv')
-
+        
         if os.path.exists(confound_file):
             print('Loading confound regressors: '+ confound_file)
             confounds_all = pd.read_csv(confound_file,sep='\t',header=0)
         else:
             print('Cant locate: ' + confound_file)
             continue
-
+        
         # TESTME - select only the desired confounds
         confounds = confounds_all.loc[:,confound_list]
         # combine with scrubbing regressors
@@ -152,14 +156,14 @@ for s,sub in enumerate(subs):
 
         # drop initial timepoints
         confounds = confounds.iloc[len(droptp): , :]
-
+                
         brain_bold_path = str.replace(analysisdir + 'vol2surf/' + sub + '_' + os.path.basename(run),'.nii.gz','_'+ hemi+'.gii')
         # vol2surf takes a while, so if we already saved the surface we can load it instead
         if os.path.exists(brain_bold_path) and not clobber:
             print(brain_bold_path + ' exists, loading...')
             brain_bold = surface.load_surf_data(brain_bold_path)
         else:
-            print('Converting volume to surface for run: ' + seed_bold_path)
+            print('Converting volume to surface for run: ' + brain_bold_path)
             # syntax for vol2surf using nilearn
             # lhbold = surface.vol_to_surf(img=bold,surf_mesh=surf, radius = 3)
             # use nsd precomputed mapping for vol2surf
@@ -182,7 +186,7 @@ for s,sub in enumerate(subs):
            # nib.freesurfer.io.write_morph_data(datadir + 'TEST.gii',run_data)
             img = nib.gifti.GiftiImage(darrays=[nib.gifti.GiftiDataArray(brain_bold)])
             nib.save(img, brain_bold_path)
-
+            
         print('Vertices by Timepoints: ')
         print(brain_bold.shape)
         if brain_bold.shape[0] < brain_bold.shape[1]:
@@ -190,7 +194,7 @@ for s,sub in enumerate(subs):
         print('Vertices by Timepoints')
         print(brain_bold.shape)
         brain_bold=np.delete(brain_bold,droptp,axis=1)
-
+        
         # load motion regressors
         # TBD - this section can be removed because now the motion regressors are saved within the confound file
 #         # We dont need to load the motion file at all, saving here in case we change the pipeline
@@ -203,10 +207,10 @@ for s,sub in enumerate(subs):
 #         else:
 #             print('Cant locate: ' + motion_file)
 #             continue
-
+        
 #         motion = motion.drop(droptp)
-
-
+        
+ 
         # Now load second hemisphere if different from seed hemisphere
         if seed_hemi == hemi:
             print('same hemisphere used for seed and whole brain\n')
@@ -247,13 +251,13 @@ for s,sub in enumerate(subs):
             print(seed_bold.shape)
             seed_bold=np.delete(seed_bold,droptp,axis=1)
 
-
+            
         # Denoise the data:
-        brain_bold = np.transpose(signal.clean(signals = np.transpose(brain_bold), filter=myfilter,high_pass=0.008, low_pass = 0.1,
+        brain_bold = np.transpose(signal.clean(signals = np.transpose(brain_bold), filter=myfilter,high_pass=0.008, low_pass = 0.1, 
                                        detrend=True, standardize='zscore',t_r=1.333, confounds =confounds))
 
 
-        seed_bold = np.transpose(signal.clean(signals = np.transpose(seed_bold), filter=myfilter,high_pass=0.008, low_pass = 0.1,
+        seed_bold = np.transpose(signal.clean(signals = np.transpose(seed_bold), filter=myfilter,high_pass=0.008, low_pass = 0.1, 
                                            detrend=True, standardize='zscore',t_r=1.333, confounds =confounds))
 
 
@@ -273,7 +277,7 @@ for s,sub in enumerate(subs):
                     continue
             else:
                 seed_timeseries = np.nanmean(seed_bold[labels==roi_labels[roi]],axis=0)
- #           seed_timeseries_c = np.nanmean(lhbold_c[labels==roi_labels[roi]],axis=0)
+ #           seed_timeseries_c = np.nanmean(lhbold_c[labels==roi_labels[roi]],axis=0)        
         # to plot
            # fig, ax = plt.subplots(figsize =(4, 3))
            # ax.plot(seed_timeseries)
@@ -283,15 +287,19 @@ for s,sub in enumerate(subs):
            # ax.set_ylabel('Normalized signal')
            # print()
 
-
-            roi_rval = np.zeros(brain_bold.shape[0])
+        
+            roi_rval = np.empty(brain_bold.shape[0])
+            roi_rval[:] = np.NaN
             for v in range(brain_bold.shape[0]):
                 roi_rval[v]=np.corrcoef(np.transpose(seed_timeseries), np.transpose(brain_bold[v]))[0,1]
 
             roi_zmap[:,r,roi] = np.arctanh(roi_rval)
 
-    #sub_filepath = analysisdir + sub + '_' + roi_names[roi] + '_' + str(len(runs)) + 'runs_' + hemi
-    sub_filepath = outputdir + sub + '_' + str(len(runs)) + 'runs_' + hemi
+    #sub_filepath = analysisdir + sub + '_' + roi_names[roi] + '_' + str(len(runs)) + 'runs_' + hemi 
+    sub_filepath = outputdir + sub + '_' + str(len(runs)) + 'runs_' + hemi 
+    if faces==True:
+        sub_filepath = outputdir + sub + '_' + str(len(runs)) + 'runs_faces_' + hemi 
+    
     np.save(sub_filepath,roi_zmap)
 
     # for plotting
@@ -299,9 +307,13 @@ for s,sub in enumerate(subs):
         h = 'left'
     elif hemi == 'rh':
         h = 'right'
-
+    
     for roi in range(len(roi_names)):
         plotting.plot_surf_stat_map(surf, stat_map=np.nanmean(roi_zmap[:,:,roi],axis=1),
         hemi=h, threshold = .2, vmax=0.7, view='lateral', colorbar=True,
         bg_map=curv,title=sub + '_' + roi_names[roi] + '_' + str(len(runs)) + 'runs', output_file = sub_filepath + '_' + roi_names[roi] + '_lat.png')
         print()
+
+
+
+
